fcb22e2f3cf0245b8e01e822053a0356e12808d9
max13h <max.hmae@gmail.com>
2023-07-07 14:37:10 +0200

    adding associations and validations to models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, presence: true , uniqueness: { scope: :list }
+  validates :list, presence: true
+  validates :name, length: { minimum: 6 }
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,18 @@
 class Movie < ApplicationRecord
   has_many :lists, through: :bookmarks
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+
+  before_destroy :check_presence
+
+  private
+
+  def check_presence
+    if bookmarks.exists?
+      errors.add(:base, message: "Cannot delete the movie because it is referenced in bookmarks.")
+      throw :abort
+    end
+  end
 end
