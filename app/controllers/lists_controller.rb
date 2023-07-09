54d3c0f6cc80d28dda78f136723345c12dd5b838
max13h <max.hmae@gmail.com>
2023-07-09 20:39:31 +0200

    adding add bookmark feature to lists + fixing style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :list_recover, only: [:show]
+  before_action :list_recover, only: [:show, :destroy]
 
   def index
     @movies = Movie.all
@@ -10,9 +10,31 @@ class ListsController < ApplicationController
     @movies = @list.movies
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def list_recover
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "lists#index"
-  get "lists/:id", to: "lists#show", as: "list"
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: "list"
+  resources :lists, only: [:new, :create, :show, :destroy] do
+    member do
+      resources :bookmarks, only: [:new, :create]
+    end
+  end
 end
